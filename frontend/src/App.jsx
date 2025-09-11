b43d423300e1c0c397306459912b8fa77e56f2cd
Nipuni Weerasuriya <nipuniweerasuriya2@gmail.com>
2025-09-11 13:52:30 +0530

    Add certificate page and component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,23 @@ import { Routes, Route, HydratedRouter } from "react-router-dom";
 import About_Us from "./pages/About_Us";
 import Course_Learning_Page from "./pages/Course_Learning_Page";
 import Learning_Progress_Page from "./pages/Learning_Progress_Page";
-
+import Certificate from "./pages/Certificate";
 
 const App = () => {
   return (
     <div className="w-full min-h-screen">
       <Routes>
-        < Route path="/About_Us" element={<About_Us />} />  
-        < Route path="/course_learning_page" element={<Course_Learning_Page />} />
-        < Route path="/Learning_Progress_Page" element={< Learning_Progress_Page />} />
+        <Route path="/About_Us" element={<About_Us />} />
+        <Route
+          path="/course_learning_page"
+          element={<Course_Learning_Page />}
+        />
+        <Route
+          path="/Learning_Progress_Page"
+          element={<Learning_Progress_Page />}
+        />
+        <Route path="/Certificate" element={<Certificate />} />
       </Routes>
-
     </div>
   );
 };
