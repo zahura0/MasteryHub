09ffd5d669d53694eb482a99c21c23361cae93e2
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-09-13 09:40:19 +0530

    updated for main branches changes of nipuni dulanka

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-  plugins: [
-    tailwindcss(),
-    react()],
+    react(),
+  ],
 })
