9d54c951819bd6aee32f796704ac634bae4ebcb0
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-09-13 08:41:35 +0530

    update hima nethmina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom";
-import ContactUs from './pages/ContactUs';
-import HelpSupportPage from './pages/HelpSupportPage';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-function App() {
+import "./App.css";
 import React from "react";
-import { Routes, Route, HydratedRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+// Layout
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Pages
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import HelpSupportPage from "./pages/HelpSupportPage";
 import About_Us from "./pages/About_Us";
 import Course_Learning_Page from "./pages/Course_Learning_Page";
 import Learning_Progress_Page from "./pages/Learning_Progress_Page";
@@ -20,28 +21,19 @@ const App = () => {
       <Navbar />
       <main className="flex-grow">
         <Routes>
+          {/* Primary routes */}
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<ContactUs />} />
-          <Route path='/help-support' element={<HelpSupportPage />} />
+          <Route path="/help-support" element={<HelpSupportPage />} />
+
+          {/* Additional routes merged into same layout */}
+          <Route path="/About_Us" element={<About_Us />} />
+          <Route path="/course_learning_page" element={<Course_Learning_Page />} />
+          <Route path="/Learning_Progress_Page" element={<Learning_Progress_Page />} />
+          <Route path="/Certificate" element={<Certificate />} />
         </Routes>
       </main>
       <Footer />
-    </div>
-  )
-}
-    <div className="w-full min-h-screen">
-      <Routes>
-        <Route path="/About_Us" element={<About_Us />} />
-        <Route
-          path="/course_learning_page"
-          element={<Course_Learning_Page />}
-        />
-        <Route
-          path="/Learning_Progress_Page"
-          element={<Learning_Progress_Page />}
-        />
-        <Route path="/Certificate" element={<Certificate />} />
-      </Routes>
     </div>
   );
 };
